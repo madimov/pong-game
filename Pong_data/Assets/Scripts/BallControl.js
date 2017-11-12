ee9454229df5f66c6599ee079c64d6711353840b
Miko Dimov <mdimov@bu.edu>
2017-11-12 01:39:00 +0100

    add pong project data and builds

---
+++ Pong_data/Assets/Scripts/BallControl.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+
+var ballSpeed : float = 100;
+
+function Start () {
+	yield WaitForSeconds(2);
+	GoBall();
+}
+
+function Update () {
+	var xVel : float = GetComponent.<Rigidbody2D>().velocity.x;
+	// Debug.Log ("Velocity = " + xVel);
+	if (xVel < 8 && xVel > -8 && xVel != 0) {
+		if (xVel > 0) {
+			GetComponent.<Rigidbody2D>().velocity.x = 10;
+		} else {
+			GetComponent.<Rigidbody2D>().velocity.x = -10;
+		}
+	// Debug.Log ("Velocity before " + xVel);
+	// Debug.Log ("Velocity after " + GetComponent.<Rigidbody2D>().velocity.x);
+	}
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+	var randomVelocity = Random.Range(0, 2);
+	if (colInfo.collider.tag == "Player") {
+		GetComponent.<Rigidbody2D>().velocity.y = GetComponent.<Rigidbody2D>().velocity.y / 2 +
+		colInfo.collider.GetComponent.<Rigidbody2D>().velocity.y / 3 + randomVelocity;
+		GetComponent.<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+		GetComponent.<AudioSource>().Play();
+	}
+}
+
+function ResetBall() {
+	GetComponent.<Rigidbody2D>().velocity.y = 0;
+	GetComponent.<Rigidbody2D>().velocity.x = 0;
+	transform.position.x = 0;
+	transform.position.y = 0;
+	
+	yield WaitForSeconds (0.5);
+	GoBall();
+}
+
+function GoBall () {
+	var randomNumber = Random.Range(0, 2);
+	if (randomNumber <= 0.5) {
+		GetComponent.<Rigidbody2D>().AddForce (new Vector2 (ballSpeed,10));
+	} else {
+		GetComponent.<Rigidbody2D>().AddForce (new Vector2 (-ballSpeed,-10));
+	}
+}
---
+++ Pong_data/Assets/Scripts/SideWalls.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function OnTriggerEnter2D (hitInfo : Collider2D) {
+	if (hitInfo.name == "Ball") {
+		var wallName = transform.name;
+		GetComponent.<AudioSource>().pitch = Random.Range(1.0f, 1.05f);
+		GetComponent.<AudioSource>().Play();
+		GameManager.Score (wallName);
+		hitInfo.gameObject.SendMessage ("ResetBall");
+	}
+}
